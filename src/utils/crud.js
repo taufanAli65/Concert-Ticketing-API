d2849dbe2960d78510bd71fcda538c5ee4f2f06a
taufanAli65 <taufanali65@gmail.com>
2024-12-31 13:23:32 +0700

    feat: add Firebase configuration and CRUD utility functions

---
+++ src/utils/crud.js
@@ -0,0 +1,59 @@
+const db = require("../firebase-config");
+
+async function add(collectionName, data) {
+  try {
+    await db.collection(collectionName).add(data);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deleteData(collectionName, docID) {
+  try {
+    await db.collection(collectionName).doc(docID).delete();
+    const message = `Document ID : ${docID} in ${collectionName} deleted successfully!`;
+    return message;
+  } catch (error) {
+      throw error;
+  }
+}
+
+async function edit(collectionName, docID, newData) {
+  try {
+    await db.collection(collection).doc(docID).update(newData);
+    const updatedDoc = await db.collection(collectionName).doc(docID).get();
+    const data = updatedDoc.data();
+    return data;
+  } catch (error) {
+      throw error;
+  }
+}
+
+async function getAll(collectionName) {
+  const data = [];
+  try {
+    const snapshot = await db.collection(collectionName).get();
+    snapshot.forEach((doc) => {
+      data.push(doc.data());
+    });
+    return data;
+  } catch (error) {
+      throw error;
+  }
+}
+
+async function get(collectionName, docID) {
+  try {
+    const docRef = db.collection(collectionName).doc(docID);
+    const docSnapshot = await docRef.get();
+    if (!docSnapshot.exists) {
+      throw new Error("Dokumen tidak ditemukan");
+    }
+    const data = docSnapshot.data();
+    return data;
+  } catch (error) {
+      throw error;
+  }
+}
+
+module.exports = { add, getAll, get, edit, deleteData };

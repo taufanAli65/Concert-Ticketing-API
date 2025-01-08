8d4b21ed12fd5c107230272e943fff292bf463a1
taufanAli65 <taufanali65@gmail.com>
2025-01-08 20:43:08 +0700

    feat: add authentication routes to the application

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 const concertRouter = require("./routes/concert");
 const ticketRouter = require("./routes/ticket");
+const authRouter = require("./routes/auth");
 
 const app = express();
 const port = process.env.APP_PORT;
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 
 app.use("/concert", concertRouter);
 app.use("/ticket", ticketRouter);
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`API is running on http://localhost:${port}`);
